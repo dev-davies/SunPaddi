37016ddd4d4badbb8f460a48a162b0d37576897c
dev-davies <dvsolaniyi@gmail.com>
2026-01-13 09:54:09 +0100

    jj

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
         name: 'SunPaddi Solar Sizer',
